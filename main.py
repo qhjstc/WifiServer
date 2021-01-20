fc4ba4d2a96346c26122e1f755b77f5a7d192890
qhjstc <654390596@qq.com>
2021-01-20 14:58:05 +0800

    It is a useful version There are still some small bugs in this version,if the client does not receive , it will be bug date:1 20 2021

--- main.py
+++ main.py
@@ -3,68 +3,95 @@
 import sys
 import socket
 import threading
+import time
 '''
 AF_INET: 基于IPv4协议的网络
 SOCK_STREAM(默认)，基于TCP协议
 SOCK_STREAM:基于TCP协议
 使用多协程的方式处理多个客户端的数据
 '''
-Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-socks = []
 
-def Server_Init():
-    print("Hello this is Server!")
-    ip_address = input("Please input the Server ip address:")
-    ip_port = int(input("Please input the Server ip port:"))
-    global  Server_ip
-    Server_ip = (ip_address, ip_port)
-    Server.bind(Server_ip)
-    Server.listen(5)             # Maximum connections 5
+class Server:         # 第一个socket是包的名字
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        self.ip_address = '0,0,0,0,'
+        self.ip_port = 0
+        self.ip = ()
+        self.number = 0
+        self.socks = []
 
+    def Server_Init(self):
+        self.ip_address = input("Please input the Server ip address:")
+        self.ip_port = int(input("Please input the Server ip port:"))
+        self.number = int(input("Please input the number of listening Clients:"))
+        self.ip = (self.ip_address, self.ip_port)
+        self.socket.bind(self.ip)
+        self.socket.listen(self.number)  # define maximum connections
 
+    def Server_Monitoring(self):               # monitoring client
+        tcheck = threading.Thread(target=self.Cheack_Heart)  # Heart threading
+        tcheck.start()
+        while True:
+            print("Enter monitoring state......")
+            print("Waiting for client {}".format(len(self.socks) + 1))
+            c = client()
+            c.conn, c.addr = self.socket.accept()
+            c.time = time.time()
+            self.socks.append(c)
+            c.threading = threading.Thread(target=c.Handle)  # child threading
+            c.threading.start()
+            print("Find the client {}".format(len(self.socks)))
+            print(c.addr)
 
-def Server_Monitoring():
-    while True:
-        print("Enter monitoring state......")
-        print("Waiting for client {}".format(len(socks) + 1))
-        conn, addr =  Server.accept()
-        conn.setblocking(0)
-        socks.append(conn)
-        print("Find the client {}".format(len(socks) + 1))
-        print(addr)
+    def Cheack_Heart(self):         # check all client heart
+        while True:
+            for c in self.socks:
+                if (time.time() - c.time) > 5:
+                    c.conn.close()
+                    self.socks.remove(c)
+                    print("{} users are offline".format(c.addr))
 
-
-def Server_Handle():              # Creat a child thread
-    while True:
-        for conn in socks:
+class client:                      # client class
+    def __init__(self):
+        self.time = 0
+        self.message = 0
+        self.conn = socket.socket()
+        self.addr = 0
+        self.threading = 0
+    def Handle(self):  # Creat a child thread
+        while True:
             try:  # Check the client whether breaks
-                client_data = conn.recv(1024)
+                self.message = self.conn.recv(1024)
             except Exception as e:
-                continue
-            if not client_data:
-                socks.remove(conn)
-                print("The client has closed")
-                continue
-
-            if client_data == b'close server\r\n':
+                print("This threading has finished")
+                break
+            self.time = time.time()
+            print(self.time)
+            if self.message == b'close server\r\n':
                 print("Close the Server")
-                Server.close()
                 print("Server has closed!\r\n")
                 sys.exit(0)
 
-            elif client_data == b'close client\r\n':
+            elif self.message == b'close client\r\n':
+                self.conn.close()
                 print("Close the Client")
-            print(client_data.decode("utf-8"))
-            conn.send('Server reply content\r\n'.encode("utf-8"))  # Reply a message to client
+                break
+
+            else:
+                print(self.message.decode("utf-8"))
+            self.conn.send('Server reply content\r\n'.encode("utf-8"))  # Reply a message to client
 
 
-t = threading.Thread(target = Server_Handle)     # child threading
+
+
+
+
 
 def main():
-    Server_Init()
-    Server_Monitoring()
-
+    s = Server()
+    s.Server_Init()
+    s.Server_Monitoring()
 
 if __name__ == "__main__":
-    t.start()
+    print("Hello this is Server!")
     main()
